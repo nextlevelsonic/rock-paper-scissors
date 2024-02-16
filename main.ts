5e6631ac1ad3f3c42d66a34d0bf122084086c0a5
nextlevelsonic <160077634+nextlevelsonic@users.noreply.github.com>
2024-02-16 09:20:51 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,76 @@
+let rock_paper_scissors = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    rock_paper_scissors = randint(1, 3)
+    if (rock_paper_scissors == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (rock_paper_scissors == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (rock_paper_scissors == 3) {
+        basic.showLeds(`
+            # # . # #
+            . # # # #
+            . . # . .
+            . # # # #
+            # # . # #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
